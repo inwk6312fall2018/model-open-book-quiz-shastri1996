65c675328db41aecf00ecefc6b603a865390e2fa
shastri1996 <kathan.shastri@dal.ca>
2018-10-09 19:19:16 -0300

    add a function where players can add or remove cards to hand one at a time

--- task1.py
+++ task1.py
@@ -14,8 +14,6 @@ else:
 	""" As per standard rule 9 players are allowed except some private poker tables """
 
 	raise myException("___MAX 9 PLAYERS CAN PLAY___") 
-
-
 def Deck_initial_stage():
 
 	""" Deck is Created  """
@@ -40,3 +38,48 @@ def Draw():
 			players.append(deck.draw())
 	print(player_lst)
 Draw()
+
+player1=player_lst[0]
+player2=player_lst[1]
+print(player1)
+print(player2)
+
+def remove_card(temp,index):
+
+	""" removes the card that player wishes  """
+	del temp[index]
+
+
+def card_took(player_no):
+
+	""" add a random card to the hand """
+	deck.shuffle()
+	player_no.append(deck.draw())
+
+def add_and_remove(player_lst):
+
+	""" players drawing new card from deck max 20 draws allowed  """
+
+	counter=0
+	
+	for i in range(0,20):
+		condition=input("DO YOU WANNA DRAW CARDS FROM DECK:Y/N??")
+		if condition=="y":
+
+
+			for players in player_lst:
+				counter+=1
+				print("PLAYER",counter%2)
+				temp=input("Do you want to choose new card?y/n")
+				#var1=int(input("enter the index of card you wanna remove: "))
+				if temp == 'y':
+					var1=int(input("Enter the index of card you wanna remove: "))
+					remove_card(players,var1)
+					card_took(players)
+					print(players)
+				else:
+					return
+	
+		else:
+			return None
+add_and_remove(player_lst)
